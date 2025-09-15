c87bf2395f0bf0914748ca8539ef759a5364b240
Abhi-mishra <abhimishra9896@gmail.com>
2025-09-15 21:06:45 +0530

    adding the dockerfile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, World!</p>
+        <p>Abhishek Mishra </p>
         <p>Hello, World!!!!!</p>
         <p>Hello, World again!!!!</p>
       </header>
